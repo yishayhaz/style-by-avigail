b36a2480e922a89c8ff63a56c6d6d47b1d6d3f82
yishayhaz <yishay11013@gmail.com>
2022-11-14 19:58:00 +0200

    navbar added

--- components/header/index.jsx
+++ components/header/index.jsx
@@ -1,12 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./style.module.scss";
 import Link from "next/link";
+import { CgMenuRight } from "react-icons/cg";
+import { IoClose } from "react-icons/io5";
 
-export default function index() {
+const routes = [
+  {
+    name: "דף הבית",
+    path: "/",
+  },
+  {
+    name: "סטיילינג אישי",
+    path: "/personal-styling",
+  },
+  {
+    name: "סדנאות",
+    path: "/workshops",
+  },
+  {
+    name: "יצירת קשר",
+    path: "/contact-me",
+  },
+];
+
+export default function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
-    <header className={styles.Header}>
-      <Link href="/contact-me">צרי קשר</Link>
-      <h1>Style By Avigail</h1>
-    </header>
+    <>
+      <div className={styles.nav_wrraper} aria-expanded={isMenuOpen}>
+        <nav>
+          {routes.map((route) => (
+            <Link href={route.path} key={route.name}>
+              {route.name}
+            </Link>
+          ))}
+        </nav>
+        <button onClick={() => setIsMenuOpen(false)}>
+          <IoClose />
+        </button>
+      </div>
+
+      <header className={styles.Header}>
+        <button className={styles.MenuBtn} onClick={() => setIsMenuOpen(true)}>
+          <CgMenuRight />
+        </button>
+        <h1>Style By Avigail</h1>
+      </header>
+    </>
   );
 }
