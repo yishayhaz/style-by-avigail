e0834bb5dc3ecf013124fa66163a91ff9cef278a
Yishay Hazan <ishay@saitechs.io>
2023-07-15 23:39:52 +0300

    fixed images

--- screens/home/ig/index.tsx
+++ screens/home/ig/index.tsx
@@ -14,22 +14,22 @@ export function IgScreen() {
       </h2>
       <div className={styles.grid}>
         <div>
-          <Image src="/IG/1.avif" alt="" width={200} height={200} />
+          <Image src="/IG/1.png" alt="" width={200} height={200} />
         </div>
         <div>
-          <Image src="/IG/2.avif" alt="" width={200} height={200} />
+          <Image src="/IG/2.png" alt="" width={200} height={200} />
         </div>
         <div>
-          <Image src="/IG/3.avif" alt="" width={200} height={200} />
+          <Image src="/IG/3.png" alt="" width={200} height={200} />
         </div>
         <div>
-          <Image src="/IG/4.avif" alt="" width={200} height={200} />
+          <Image src="/IG/4.png" alt="" width={200} height={200} />
         </div>
         <div>
-          <Image src="/IG/5.avif" alt="" width={200} height={200} />
+          <Image src="/IG/5.png" alt="" width={200} height={200} />
         </div>
         <div>
-          <Image src="/IG/6.avif" alt="" width={200} height={200} />
+          <Image src="/IG/6.png" alt="" width={200} height={200} />
         </div>
       </div>
     </section>
